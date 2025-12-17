a1ac87d80a890fb31e6431b70eaac55a07775bbe
Vinícius D Saraiva <viniciusdsaraiva@gmail.com>
2025-12-16 23:34:36 -0300

    fix audio compoenent in simple enemy

--- scripts/listeners/audio_component.gd
+++ scripts/listeners/audio_component.gd
@@ -1,14 +1,22 @@
 class_name AudioComponent
 extends Node
 
-@export var state_machine: StateMachine
+@export var e_state_machine: StateMachine
+@export var e_simple_state_machine: SimpleStateMachine
 @export var footstep_sfx: AudioStream
 
 func _ready():
+	var state_machine: Node
+	if e_state_machine:
+		state_machine = e_state_machine
+	elif e_simple_state_machine:
+		print("e_simple_state_machine")
+		state_machine = e_simple_state_machine
 	assert(state_machine != null, "AudioComponent: StateMachine não foi atribuída no Inspetor.")
 	state_machine.phase_changed.connect(_on_phase_changed)
 
 func _on_phase_changed(phase_data: Dictionary):
+	print("SOUND: ", phase_data)
 	if phase_data.has("sfx_to_play"):
 		var sfx = phase_data["sfx_to_play"]
 		if sfx is AudioStream:
